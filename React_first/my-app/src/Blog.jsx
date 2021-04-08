b15fcffb3bb6ba5ebaed10ba5e5a178b555924f7
ueuchi <80137361+ueuchi@users.noreply.github.com>
2021-04-08 21:45:42 +0900

    第５回

--- React_first/my-app/src/Blog.jsx
+++ React_first/my-app/src/Blog.jsx
@@ -4,14 +4,24 @@ import Article from './Article'
 class Blog extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isPublished: false,
+            order: 1
+        }
     }
+
+// 公開状態を反転させる関数
+togglePublished = () => {
+    this.setState( {
+        isPublished: !this.state.isPublished
+    })
+}
+
     render() {
         const authorName = "Torahack"
         return (
             <div>
-                <Article title={"Reactの使い方"} />
-                <Article title={"JSXの使い方"} />
-                <Article title={"環境構築をしてみよう"} />
+                <Article title={"Reactの使い方"} isPublished={this.state.isPublished} toggle={() => this.togglePublished()}/>
             </div>
         )
     }
