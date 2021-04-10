d00b515c0a6507b648411c04020b261dc85ca834
ueuchi <80137361+ueuchi@users.noreply.github.com>
2021-04-10 22:07:22 +0900

    第九回

--- React_first/my-app/src/Article.jsx
+++ React_first/my-app/src/Article.jsx
@@ -9,7 +9,7 @@ const Article = (props) => {
             <h2>{props.title}</h2>
             <label htmlFor="check">公開状態：</label>
             <input type="checkbox" chacked={isPublished} id="check" onClick={() => togglePublished(!isPublished)}/>
-            {/* <LikeButton count={props.count}/> */}
+            <LikeButton />
         </div>
     )
 }
