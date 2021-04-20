5d0942b282722d30236530a1cb604d6fa0bf9586
ueuchi <80137361+ueuchi@users.noreply.github.com>
2021-04-20 20:30:19 +0900

    とらゼミ第４回

---
+++ React_first/chatbot-demo/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import defaultDataset from "./dataset";
+import './assets/styles/style.css';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      answers: [],
+      chats: [],
+      currentId: "init",
+      dataset: defaultDataset,
+      open: false
+    }
+  }
+
+    render(){
+      return (
+        <div>
+          <section className="c-section">
+            <div className="c-box">
+              
+
+            </div>
+          </section>
+        </div>
+      );
+    }
+
+}
+
+
