3edd644649e56b9e95f2a46970661f6117df52df
ueuchi <80137361+ueuchi@users.noreply.github.com>
2021-05-28 10:19:42 +0900

    useState勉強

--- React_first/react_humm/src/App.js
+++ React_first/react_humm/src/App.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  const increment2 = () => {
+    setCount(previousCount => previousCount + 1)
+  }
+  const decrement2 = () => {
+    setCount(previousCount => previousCount - 1)
+  }
+
+  const hundleReset = () => {
+    setCount(0)
+  }
+  const double = () => {
+    setCount(count * 2)
+  }
+  const triple = () => {
+    count % 3 === 0 ? setCount(count / 3) : setCount(count)
+  }
+  // const triple = () => {
+  //   if(count % 3 === 0) {
+  //     setCount(count / 3)
+  //   }
+  // }
+
   return (
+    <>
+    <div>count: {count}</div>
     <div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
     </div>
+    <div>
+      <button onClick={increment2}>+1</button>
+      <button onClick={decrement2}>-1</button>
+    </div>
+    <div>
+      <button onClick={hundleReset}>Reset</button>
+      <button onClick={double}>x2</button>
+      <button onClick={triple}>３の倍数の時だけ３で割る</button>
+    </div>
+    </>
   );
 }
 
