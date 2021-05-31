35c589803210415beb41ae8d5ba88ce903afdf01
ueuchi <80137361+ueuchi@users.noreply.github.com>
2021-05-31 20:18:53 +0900

    implement callbacks with useEffect

--- React_first/react_humm/src/App.js
+++ React_first/react_humm/src/App.js
@@ -1,22 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const App = (props) => {
-  const [name, setName] =useState(props.name)
-  const [price, setPrice] =useState(props.price)
+  const [state, setState] =useState(props)
+  //name,priceをstateに置き換えることでstate.nameのような記述を省略できる。
+  const {name, price} = state
 
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
+  // 毎回renderされる
+  useEffect(() => {
+    console.log('this is like componentDidMount or componentDidUpdata')
+  })
 
-  }
+  //最後に記述した配列内の要素が変わるまでrenderされない。
+  useEffect(() => {
+    console.log('This callback is for name only.')
+  }, [name])
   
   return (
     <>
       <p>現在の{name}は、{price}円です。</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={e => setName(e.target.value)}/>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => {setState(props)}}>Reset</button>
+      <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
     </>
   );
 }
