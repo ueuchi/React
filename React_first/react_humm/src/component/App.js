1ec6047ed050923cd22af2f83a8150a8c27b3a2d
ueuchi <80137361+ueuchi@users.noreply.github.com>
2021-06-02 08:05:05 +0900

    app.js修正

--- React_first/react_humm/src/component/App.js
+++ React_first/react_humm/src/component/App.js
@@ -1,33 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = (props) => {
-  const [state, setState] =useState(props)
-  //name,priceをstateに置き換えることでstate.nameのような記述を省略できる。
-  const {name, price} = state
-
-  // 毎回renderされる
-  useEffect(() => {
-    console.log('this is like componentDidMount or componentDidUpdata')
-  })
-
-  //最後に記述した配列内の要素が変わるまでrenderされない。
-  useEffect(() => {
-    console.log('This callback is for name only.')
-  }, [name])
-  
+const App = () => {
   return (
-    <>
-      <p>現在の{name}は、{price}円です。</p>
-      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
-      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
-      <button onClick={() => {setState(props)}}>Reset</button>
-      <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
-    </>
+    <div className="container-fluid">
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          {/* labelとidの名前を一緒にしないとタイトルをクリックした時にinputタグにフォーカスが当たらない */}
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input className="form-control" id="formEventTitle"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">ボディー</label>
+          <textarea className="form-control" id="formEventTitle"/>
+        </div>
+        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <h4>イベント一覧</h4>
+        <table className="table table-hover">
+          <thead>
+              <tr>
+                <th>ID</th>
+                <th>タイトル</th>
+                <th>ボディー</th>
+                <th>削除</th>
+              </tr>
+          </thead>
+          <tbody>
+          </tbody>
+
+        </table>
+      </form>
+    </div>
   );
-}
-App.defaultProps = {
-  name: '',
-  price: 1000
 }
 
 export default App;
